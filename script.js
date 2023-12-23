f41b4a8e11343277a068f39ba998bdc3ecf83eac
James Woods <122772824+JamesWoods704@users.noreply.github.com>
2023-12-22 22:29:39 -0800

    added randomizing logic and pushing list into empty array

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 let generateBtn = document.querySelector("#generate");
 
+function randomint(min, max) {
+  if (!max) {
+    max = min
+    min = 0
+  }
+
+  let rand = Math.random()
+  return Math.floor(min * (1 - rand) + rand * max)
+}
+
+
+function getrandomitem(list) {
+  return list[randomint(list.length)]
+}
+
 function generatepassword() {
     let Input = prompt("how many characters do you want in your password")
     let length = parseInt(Input)
@@ -21,5 +36,27 @@ function generatepassword() {
     let lowercase = confirm("do you want lowercase")
     let uppercase = confirm("do you want uppercase")
     let symbol = confirm("do you want a symbol")
-}
 
+    let numbersList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    let lowercaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    let uppercaseList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    let symbolsList = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "{", "]", "}", "|", ";", ":", "'", "'", ",", "<", ".", ">", "/", "?"]
+
+    let password = []
+
+    if (numbers === true) {
+      password.push(numbersList)
+    }
+    if (lowercase ===  true) {
+      password.push(lowercaseList)
+    }
+    if (uppercase === true) {
+      password.push(uppercaseList)
+    }
+    if (symbol === true) {
+      password.push(symbolsList)
+    }
+
+    let generatedPassword = ""
+
+  }
